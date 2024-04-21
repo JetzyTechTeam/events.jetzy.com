a73ad99f8a97eecf6a138b6f0e40985734df9cb4
Obi Pascal <pascalobi83@gmail.com>
2024-04-21 16:24:31 +0100

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "@Jetzy/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
