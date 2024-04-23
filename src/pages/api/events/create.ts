3875b52eb8ab18396bb67cd5db7449d581802888
Obi Pascal <pascalobi83@gmail.com>
2024-04-23 11:30:37 +0100

    updated

--- src/pages/api/events/create.ts
+++ src/pages/api/events/create.ts
@@ -1,18 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { sendResponse } from "@Jetzy/lib/helpers"
+import { generateRandomId, sendResponse } from "@Jetzy/lib/helpers"
 import { ResCode } from "@Jetzy/lib/responseCodes"
-import { Users } from "@Jetzy/models/userModal"
-import { Roles } from "@Jetzy/types"
 import type { NextApiRequest, NextApiResponse } from "next"
-import bcrypt from "bcrypt"
 import { Events } from "@Jetzy/models/eventsModal"
-
-type Data = {
-  firstName: string
-  lastName: string
-  email: string
-  password: string
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   try {
@@ -22,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     if (await Events.findOne({ name: name?.toLowerCase() }).exec()) return sendResponse(res, null, "Event already exist.", false, ResCode.BAD_REQUEST)
 
     //  create event
-    const event = await Events.create({ name, datetime, location, interest: interest?.toString()?.split(", "), privacy, isPaid, amount, desc, image })
+    const event = await Events.create({ slug: generateRandomId(8, false), name, datetime, location, interest: interest?.toString()?.split(", "), privacy, isPaid, amount, desc, image })
     if (!event) return sendResponse(res, null, "Failed to create event.", false, ResCode.INTERNAL_SERVER_ERROR)
 
     return sendResponse(res, event, "Event created successfully!", true, ResCode.CREATED)
