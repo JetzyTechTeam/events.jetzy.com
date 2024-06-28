1c4653dc4c7ff676a5d3032acbde0e92d2778f2b
Obi Pascal Banjuare <pascalobi83@gmail.com>
2024-06-28 18:51:10 +0100

    update: Add klook affilate plugin

--- src/redux/reducers/ticketsSlice.ts
+++ src/redux/reducers/ticketsSlice.ts
@@ -1,57 +1,66 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { HYDRATE } from "next-redux-wrapper"
-import { TicketSliceState, RequestParams, CreateTicketFormData, TicketInterface } from "@Jetzy/types"
+import {
+	TicketSliceState,
+	RequestParams,
+	CreateTicketFormData,
+	TicketInterface,
+	CreateTicketInterfaceResponse,
+} from "@Jetzy/types"
 import { ServerErrors, Success } from "@Jetzy/lib/_toaster"
 import { AppState } from "../stores"
 import { CreateTicketApi } from "@Jetzy/services/tickets/ticketsapis"
 
-export const CreateTicketThunk = createAsyncThunk("tickets/createTicket", async (params: RequestParams<CreateTicketFormData>, thunkApi) => {
-  return await CreateTicketApi(params)
-})
+export const CreateTicketThunk = createAsyncThunk(
+	"tickets/createTicket",
+	async (params: RequestParams<CreateTicketFormData>, thunkApi) => {
+		return await CreateTicketApi(params)
+	},
+)
 
 // Initial state
-const initialState: TicketSliceState<TicketInterface> = {
-  isLoading: false,
-  data: undefined,
+const initialState: TicketSliceState<CreateTicketInterfaceResponse> = {
+	isLoading: false,
+	data: undefined,
 }
 
 // Actual Slice
 export const ticketSlice = createSlice({
-  name: "tickets",
-  initialState,
-  reducers: {
-    // Special reducer for hydrating the state. Special case for next-redux-wrapper
-    extraReducers: {
-      // @ts-ignore
-      [HYDRATE]: (state, action) => {
-        return {
-          ...state,
-          ...action.payload,
-        }
-      },
-    },
-  },
+	name: "tickets",
+	initialState,
+	reducers: {
+		// Special reducer for hydrating the state. Special case for next-redux-wrapper
+		extraReducers: {
+			// @ts-ignore
+			[HYDRATE]: (state, action) => {
+				return {
+					...state,
+					...action.payload,
+				}
+			},
+		},
+	},
 
-  extraReducers(builder) {
-    builder.addCase(CreateTicketThunk.pending, (state) => {
-      state.isLoading = true
-    })
+	extraReducers(builder) {
+		builder.addCase(CreateTicketThunk.pending, (state) => {
+			state.isLoading = true
+		})
 
-    builder.addCase(CreateTicketThunk.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.ticket = action.payload?.data
+		builder.addCase(CreateTicketThunk.fulfilled, (state, action) => {
+			state.isLoading = false
+			state.ticket = action.payload?.data
 
-      if (action.payload?.status) {
-        Success("Ticket created.", "Ticket created successfully!")
-        state.data = action.payload?.data
-      }
-    })
+			if (action.payload?.status) {
+				Success("Ticket created.", "Ticket created successfully!")
+				state.data = action.payload?.data
+			}
+		})
 
-    builder.addCase(CreateTicketThunk.rejected, (state, action) => {
-      state.isLoading = false
-      ServerErrors("Couldn't create ticket.", action.error)
-    })
-  },
+		builder.addCase(CreateTicketThunk.rejected, (state, action) => {
+			state.isLoading = false
+			ServerErrors("Couldn't create ticket.", action.error)
+		})
+	},
 })
 
 export const {} = ticketSlice.actions
